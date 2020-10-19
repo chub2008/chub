e02817398e612eca7a690736c49f9f052556b5b4
chubin <chubin>
2020-10-19 11:31:19 +0800

    2020-10-19，添加ChubTest1测试类。

---
+++ chub-test1/src/main/java/ChubTest1.java
@@ -0,0 +1,22 @@
+/**
+ * @file name      : ChubTest1
+ * @describe : 例如:前置程序API函数
+ * @version : 例如:V3.0.1
+ * @author : chub
+ * @created on     : 2020-10-19
+ * @last modified  : 2020-10-19
+ * @module purpose : 例如:前置程序所用的函数
+ * @This module contains the following functions and functional descriptions :
+ * (1) int   fun0(); 这是模板，请注意修改。
+ * (2) char* fun1(); 这是模板，请注意修改。
+ * @Modification record :
+ * (1)  2018-12-02  edition V1.00, totle 523  lines; 这是模板，请注意修改。
+ * (2)  2018-12-21  edition V2.01, totle 1024 lines; 这是模板，请注意修改。
+ */
+
+public class ChubTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test class!");
+    }
+}
